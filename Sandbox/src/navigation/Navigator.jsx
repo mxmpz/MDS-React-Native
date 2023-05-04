98f490200a135035c87817c302f0904a16718308
Maxime Prouzat <maximeprouzat6@gmail.com>
2023-05-04 11:16:39 +0200

    Stack and TabStack navigator

--- Sandbox/src/navigation/Navigator.jsx
+++ Sandbox/src/navigation/Navigator.jsx
@@ -2,16 +2,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/Home'
 import Profile from '../screens/Profile'
 import Map from '../screens/Map'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 const Stack = createNativeStackNavigator()
+const TabStack = createBottomTabNavigator()
 
-function Navigator () {
+function ProfileNavigator () {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Home' component={Home} />
       <Stack.Screen name='Profile' component={Profile} />
       <Stack.Screen name='Map' component={Map} />
     </Stack.Navigator>
+  )
+}
+
+function Navigator () {
+  return (
+    <TabStack.Navigator initialRouteName='Home'>
+      <TabStack.Screen name='Home' component={Home} />
+      <TabStack.Screen options={{ headerShown: false }} name='ProfileStack' component={ProfileNavigator} />
+    </TabStack.Navigator>
   )
 }
 
